0a47d2ad22a8b44fc7ba7319e24b459fd1645d4b
Denchik <d.opukich@mail.ru>
2024-08-01 14:48:46 +0900

    Update BlastedImageModule.java

--- android/src/main/java/com/reactlibrary/BlastedImageModule.java
+++ android/src/main/java/com/reactlibrary/BlastedImageModule.java
@@ -28,8 +28,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.Color;
 
 import android.util.Log;
 
@@ -98,7 +96,6 @@ public class BlastedImageModule extends ReactContextBaseJavaModule {
 
             Glide.with(getReactApplicationContext())
                 .load(glideUrl)
-                .placeholder(new ColorDrawable(Color.BLACK))
                 .apply(requestOptions)
                 .listener(new RequestListener<Drawable>() {
                     @Override
